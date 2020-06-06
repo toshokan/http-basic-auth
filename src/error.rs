b95b8b62f89326c60f439aceab42fd984fd994ba
toshokan <toshokan@shojigate.net>
2020-06-06 14:04:20 -0400

    Bad scheme error, utf8 error

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,18 @@
+#[derive(Debug, PartialEq)]
 pub enum ParseError {
+    Scheme,
     Decode,
     Format
 }
 
 impl From<base64::DecodeError> for ParseError {
     fn from(_: base64::DecodeError) -> Self {
+	ParseError::Decode
+    }
+}
+
+impl From<std::str::Utf8Error> for ParseError {
+    fn from(_: std::str::Utf8Error) -> Self {
 	ParseError::Decode
     }
 }
